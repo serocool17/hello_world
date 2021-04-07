74be711a05393fbb877e49e5c5a47820d15384c9
serocool17 <richardceballo@gmail.com>
2021-04-07 15:20:00 -0400

    First project including user, date and time, account info.

--- src/ca/concordia/id40181099/helloworldapp/Main.java
+++ src/ca/concordia/id40181099/helloworldapp/Main.java
@@ -1,19 +1,52 @@
+
 package ca.concordia.id40181099.helloworldapp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Main {
+
+
+    private static Object LocalTime;
 
     public static void main(String[] args) {
 	// My first project
 
-        /* This is a very nice code */
+        // String functions
 
-        String First = ("Jhon");
-        String Last = ("Smith");
+        String Bank = ("TD Canada");
+        String welcome = ("Hello");
+        String First = ("Richardson");
+        String Last = ("Ceballo");
+        String Balance = ("Your account balance is:");
+        String transactionDate = ("Transaction Date:");
 
-        System.out.println("HELLO");
-        System.out.println("WORLD");
+        //Integer values and declarations
+        int age = 25;
+        int Account = 1457;
+        int payment = 2000;
+        int total = (Account - payment);
 
-        System.out.println(First + " " + Last);
+        //Local date and time with format
+
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter date1 = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String formattedDate = date.format(date1);
+
+
+        //Elements to be print
+
+        System.out.println(Bank);
+        System.out.println(transactionDate + " " + formattedDate);
+        System.out.println(welcome + " " + First + " " + Last + " " + Balance);
+        System.out.println(Account + " " + "$" + payment + " " + "$"+ total);
+
+
+
+
+
+
 
 
     }
