5ce616ad050e9f6d5c125b86e89330b73870343a
serocool17 <richardceballo@gmail.com>
2021-04-06 21:59:53 -0400

    First project

---
+++ src/ca/concordia/id40181099/helloworldapp/Main.java
@@ -0,0 +1,22 @@
+package ca.concordia.id40181099.helloworldapp;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// My first project
+
+        /* This is a very nice code */
+
+        String First = ("Jhon");
+        String Last = ("Smith");
+
+        System.out.println("HELLO");
+        System.out.println("WORLD");
+
+        System.out.println(First + " " + Last);
+
+
+    }
+
+
+}
